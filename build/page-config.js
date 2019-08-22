4bf2e7e71628d2f80fa0f96d0f62cf23d05653f3
scuechjr <sucechjr@163.com>
2019-08-22 11:33:58 +0800

    模板修改

---
+++ build/page-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    live2d: {
+        entry: './src/pages/live2d/index.js',
+        template: './src/pages/live2d/index.html'
+    },
+    webcat: {
+        entry: './src/pages/webcat/index.js',
+        template: './src/pages/webcat/index.html'
+    },
+    home: {
+      entry: './src/index.js',
+      template: './src/index.html'
+    }
+};
